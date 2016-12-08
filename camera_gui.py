1a904c31e870e4ddfc2f4bb0fc2c713b0a287316
Romain <romain.brette@inserm.fr>
2016-12-08 10:38:06 +0100

    Camera GUI shows image with a target plus.

--- camera_gui.py
+++ camera_gui.py
@@ -14,12 +14,15 @@ while(True):
     width, height, _ = frame.shape
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    frame = cv2.line(frame, (width/2-10, height/2-10), (width/2+10, height/2+10), (255, 255, 255), 5)
+    cv2.line(frame, (height/2, width/2-10), (height/2, width/2+10), (0, 0, 255))
+    cv2.line(frame, (height/2-10, width/2), (height/2+10, width/2), (0, 0, 255))
+
+    #print frame
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('Camera',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
