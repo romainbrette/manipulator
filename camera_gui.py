adbcde907a56f911758f2b3dcb55868fe67e22f6
Romain <romain.brette@inserm.fr>
2016-12-08 16:55:21 +0100

    camera_gui can save videos

--- camera_gui.py
+++ camera_gui.py
@@ -8,13 +8,26 @@ it seems that OpenCV sets it to 640x480 by default (maybe could be set)
 import numpy as np
 import cv2
 
+from os.path import expanduser
+home = expanduser("~")
+filename = home+'/video.avi'
+
 def click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print x,y
 
 cap = cv2.VideoCapture(0)
+width = int(cap.get(3))
+height = int(cap.get(4))
+print width,height
+
 cv2.namedWindow('Camera')
 cv2.setMouseCallback("Camera", click)
+
+fourcc = cv2.cv.CV_FOURCC(*'mp4v')
+video = cv2.VideoWriter(filename,fourcc,20.,(width,height), True)
+
+save = False
 
 while(True):
     # Capture frame-by-frame
@@ -23,16 +36,24 @@ while(True):
     width, height, _ = frame.shape
 
     # Our operations on the frame come here
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if save:
+        video.write(frame)
 
     cv2.line(frame, (height/2, width/2-10), (height/2, width/2+10), (0, 0, 255))
     cv2.line(frame, (height/2-10, width/2), (height/2+10, width/2), (0, 0, 255))
 
     # Display the resulting frame
     cv2.imshow('Camera',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
         break
+    if key & 0xFF == ord('s'):
+        save = not save
+        print "Save =",save
 
 # When everything done, release the capture
 cap.release()
+video.release()
 cv2.destroyAllWindows()
