e415a02730ce9ab927e416afa2869056db940a0d
achagnon <mra.chagnon@gmail.com>
2017-05-12 16:21:55 +0200

    Auto calibration in progress

--- auto_calibration.py
+++ auto_calibration.py
@@ -1,66 +1,90 @@
 """
-Automated calibration of the manipulator
+Camera GUI for LuigsNeumann_SM10 and SM5
+Requires OpenCV
+To use autofocus, the user shall put the tip on focus in the displayed rectangle and press 't'
+Autofocus using template matching by pressing 'f'
+Display a rectangle around the detected tip
+Quit with key 'q'
 """
 
 from autofocus import *
-from devices import *
-import cv2
-from math import atan2
-from serial import SerialException
 
-# Capture Video input
-cap = cv2.VideoCapture(0)
-cv2.namedWindow('Camera')
+# Type of used controller, either 'SM5' or 'SM10 for L&N SM-5 or L&N SM-10
+devtype = 'SM10'
 
-# Devices to control
-try:
-    dev = LuigsNeumann_SM10()
-except SerialException:
-    print "L&N SM-10 not found. Falling back on fake device."
-    dev = FakeDevice()
+# Initializing the device, camera and microscope according to the used controller
+dev, microscope, cap = init_device(devtype)
 
-microscope = XYZUnit(dev, [7, 8, 9])
+# Device controlling the used arm
 arm = XYZUnit(dev, [1, 2, 3])
 
-init_pos_m = (microscope.position(0), microscope.position(1), microscope.position(2))
-init_pos_a = (arm.position(0), arm.position(1), arm.position(2))
+# Naming the live camera window
+cv2.namedWindow('Camera')
 
-# step case
-step = -1
+# Booleans for tracking mode and existence of the template image for autofocus
+template = 0
+calibrate = 0
+step = 0
 
-frame, img = getImg(cap)
+# GUI loop with image processing
 while 1:
 
+    # Capture a frame from video
+    if devtype == 'SM5':
+        buffer = microscope.getLastImage()
+
+    frame, img = getImg(devtype, microscope, cv2cap=cap)
+
+    # Keyboards controls:
+    # 'q' to quit,
+    # 'f' for autofocus,
+    # 't' to take the template image,
+    # 'd' to make/stop a move with focus tracking
     key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
 
-    if step == -1:
-        if key & 0xFF == ord('b'):
+    if key & 0xFF == ord('f'):
+        try:
+            maxval = focus(devtype, microscope, template, cap)
+            print maxval
+            print 'Autofocus done.'
+        except TypeError:
+            print 'Template image has not been taken yet.'
+
+    if key & 0xFF == ord('b'):
+        calibrate ^= 1
+
+    if calibrate:
+        if step == 0:
+            #template = img[height / 2 - 20:height / 2 + 20, width / 2 - 20:width / 2 + 20]
+            template = get_template(img)
+            cv2.imshow('template', template)
             step += 1
-    elif step == 0:
-        tipfocus(microscope, cap)
-        x0, y0, c = tip_detect(img)
-        focus_track(arm, 50, 0, microscope, cap) # refaire ici pour rafraichir l'affichage durant le mouvement
-        step += 1
-    elif step == 1:
-        x1, y1, c = tip_detect(img)
-        z_diff = microscope.position(2) - init_pos_m[2]
-        theta1 = atan2((50**2-z_diff**2)**0.5, z_diff)
-        theta2 = atan2(y1-y0, x1-x0)
-        arm.relative_move(50, 1)
-        step += 1
-        print theta1
-    elif step == 2:
-        x2, y2, c = tip_detect(img)
-        microscope.relative_move(50, 0)
-        step += 1
-    elif step == 3:
-        x3, y3, c = tip_detect(img)
-        microscope.relative_move(50, 1)
-        step += 1
+        elif step == 1:
+            # algo romain
+            step += 1
+        elif step == 2:
+            # move XY platform
+            calibrate = 0
+
+    # Our operations on the frame come here
+    if type(template) == int:
+        # Display a rectangle where the template will be taken
+        frame = disp_template_zone(frame)
     else:
-        pass
+        # Display a rectangle at the template matched location
+        res, maxval, maxloc = templatematching(img, template)
+        # print maxval
+        if res:
+            x, y = maxloc[:2]
+            cv2.rectangle(frame, (x, y), (x + template.shape[0], y + template.shape[1]), (0, 255, 0))
 
+    # Reversing the frame, FIND SOMETHING SO IT WORKS WITH TEMPLATE (REVERSE TEMPLATE ?)
+    #frame = cv2.flip(frame, 2)
 
-    frame, img = cap.read()
-
+    # Display the resulting frame
     cv2.imshow('Camera', frame)
+
+# When everything done, release the capture
+stop_device(devtype, dev, microscope, cap)
