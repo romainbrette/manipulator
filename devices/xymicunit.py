30f819112921001ef99e6bd5f72683dc3aee683d
achagnon <mra.chagnon@gmail.com>
2017-05-15 15:52:46 +0200

    A XYMicUnit class to make a compound unit stage + microscope Z

---
+++ devices/xymicunit.py
@@ -0,0 +1,100 @@
+"""
+An XYZ unit made of an XY stage and another device representing the microscope Z axis
+"""
+from device import *
+
+__all__ = ['XYMicUnit']
+
+
+class XYZUnit(Device):
+    def __init__(self, dev, dev_mic, axes):
+        '''
+        Parameters
+        ----------
+        dev : underlying device for the XY stage
+        dev_mic : underlying device for the Z axis
+        axes : list of 2 axis indexes
+        '''
+        Device.__init__(self)
+        self.dev = dev
+        self.dev_mic = dev_mic
+        self.axes = axes
+        self.memory = dict() # A dictionary of positions
+
+    def position(self, axis = None):
+        '''
+        Current position along an axis.
+
+        Parameters
+        ----------
+        axis : axis number starting at 0; if None, all XYZ axes
+
+        Returns
+        -------
+        The current position of the device axis in um.
+        '''
+        if axis is None: # all positions in a vector
+            return self.dev.position_group(self.axes) + [self.dev_mic.position()]
+        else:
+            if axis == 2: # Z
+                return self.dev_mic.position()
+            else:
+                return self.dev.position(self.axes[axis])
+
+    def absolute_move(self, x, axis = None):
+        '''
+        Moves the device axis to position x in um.
+
+        Parameters
+        ----------
+        axis : axis number starting at 0; if None, all XYZ axes
+        x : target position in um.
+        '''
+        if axis is None:
+            # then we move all axes
+            self.dev.absolute_move_group(x, self.axes)
+            self.dev_mic.absolute_move(x)
+        else:
+            if axis == 2: # Z
+                self.dev_mic.absolute_move(x)
+            else:
+                self.dev.absolute_move(x, self.axes[axis])
+
+    def relative_move(self, x, axis = None):
+        '''
+        Moves the device axis by relative amount x in um.
+
+        Parameters
+        ----------
+        axis : axis number starting at 0; if None, all XYZ axes
+        x : position shift in um.
+        '''
+        if axis is None:
+            self.dev.relative_move_group(x, self.axes)
+            self.dev_mic.relative_move(x)
+        else:
+            if axis == 2: # Z
+                self.dev_mic.relative_move(x)
+            else:
+                self.dev.relative_move(x, self.axes[axis])
+
+    def save(self, name):
+        self.memory[name] = self.position()
+
+    def load(self, name):
+        self.absolute_move(self.memory[name])
+
+    def stop(self, axis = None):
+        """
+        Stop current movements.
+        """
+        if axis is None:
+            # then we stop all axes
+            for i, axis in enumerate(self.axes):
+                self.dev.stop(axis)
+            self.dev_mic.stop()
+        else:
+            if axis == 2:
+                self.dev_mic.stop()
+            else:
+                self.dev.stop(self.axes[axis])
