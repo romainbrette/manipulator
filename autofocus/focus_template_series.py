a23a388075967e9b9eb2d8647e3cf38394cbcd4e
achagnon <mra.chagnon@gmail.com>
2017-05-19 15:56:30 +0200

    Should be possible to focus using a serie of template images. To be tested.

---
+++ autofocus/focus_template_series.py
@@ -0,0 +1,74 @@
+"""
+Autofocusing on the tip using template matching
+First user must put the tip on focus to get the template
+Template matching is not scale nor rotation invariant
+"""
+
+__all__ = ['focus']
+
+from template_matching import *
+from get_img import *
+import cv2
+
+
+
+def focus(devtype, microscope, template, cv2cap=None):
+    """
+    Autofocus by searching the best template match in the image around the current height
+    :param devtype: type of used controller
+    :param microscope: device controlling the microscope
+    :param template: tab of template images to look for
+    :param cv2cap: video capture from cv2, unnecessary if devtype='SM5' 
+    :param step: step length made if the arm is moving (for quicker tracking)
+    """
+
+    # Getting the microscope height according to the used controller
+    if devtype == 'SM5':
+        current_z = microscope.getPosition()
+    elif devtype== 'SM10':
+        current_z = microscope.position(2)
+    else:
+        raise TypeError('Unknown device. Should be either "SM5" or "SM10".')
+
+    frame, img, cv2cap = getImg(devtype, microscope, current_z, cv2cap)
+    # Tabs of maxval and their location during the process
+    vals = []
+    locs = []
+
+    # Getting the maxvals and their locations at +- rng um, 1um steps, around the current height
+    for i in template:
+
+        res, val, loc = templatematching(img, i)
+        locs += [loc]
+
+        if res:
+            # Template has been detected
+            vals += [val]
+        else:
+            # Template has not been detected, val set at 0
+            vals += [0]
+        '''
+        if val > 0.95:
+            maxval = val
+            dep = rng - i
+            #print len(vals)
+            return maxval, dep, loc, frame
+        '''
+    print vals
+    # Search of the highest value, indicating which template image match the best the current image
+    maxval = max(vals)
+
+    if maxval != 0:
+        # At least one template has been detected, setting the microscope at corresponding height
+        index = vals.index(maxval)
+        loc = locs[index]
+        focus_height = current_z + index - len(template)/2
+        frame , _, cv2cap = getImg(devtype, microscope, focus_height , cv2cap)
+        cv2.imshow('Camera', frame)
+        cv2.waitKey(1)
+        dep = index- len(template)/2
+    else:
+        # No template has been detected, focus can not be achieved
+        raise ValueError('The template image has not been detected.')
+
+    return maxval, dep, loc, frame, cv2cap
---
+++ autofocus/get_template_series.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+from devices import *
+from get_img import *
+
+
+def get_template_series(devtype, microscope, nb_images, cap):
+    """
+    Get a template image of the tip around the center of an image for any angle of the tip.
+    """
+
+    template_series = []
+
+    frame, img, cap = getImg(devtype, microscope, cv2cap=cap)
+    height, width = img.shape[:2]
+    ratio = 32
+
+    template = img[height/2-3*height/ratio:height/2+3*height/ratio, width/2-3*width/ratio:width/2+3*width/ratio]
+    height, width = template.shape[:2]
+    weight = []
+    for i in range(3):
+        for j in range(3):
+            temp = template[i*height/4:height/2+i*height/4, j*width/4:width/2+j*width/4]
+            bin_edge, _ = np.histogram(temp.flatten())
+            #weight += [temp.argmax()]
+            weight += [bin_edge.max()]
+
+    index = weight.index(max(weight))
+    index = 5
+    j = index%3
+    i = index//3
+
+    if devtype == 'SM5':
+        pos = microscope.getPosition()
+    elif devtype== 'SM10':
+        pos = microscope.position(2)
+    else:
+        raise TypeError('Unknown device. Should be either "SM5" or "SM10".')
+
+    for k in range(nb_images):
+        frame, img, cap = getImg(devtype, microscope, pos-(nb_images-1)/2+k, cap)
+        img = img[height / 2 - 3 * height / ratio:height / 2 + 3 * height / ratio,
+              width / 2 - 3 * width / ratio:width / 2 + 3 * width / ratio]
+        img = img[i * height / 4:height / 2 + i * height / 4, j * width / 4:width / 2 + j * width / 4]
+        template_series += [img]
+
+
+    return template_series
+
+def disp_template_zone(img):
+    """
+    Display the zone where the template image is taken
+    :param img: image
+    :return: img: image with a red rectangle and a red centered cross
+    """
+    height, width = img.shape[:2]
+    ratio = 32
+    pt1 = (width/2 - 3*width/ratio, height/2 - 3*height/ratio)
+    pt2 = (width/2 + 3*width/ratio, height/2 + 3*height/ratio)
+    cv2.rectangle(img, pt1, pt2, (0, 0, 255))
+    cv2.line(img, (width / 2 + 10, height / 2), (width / 2 - 10, height / 2), (0, 0, 255))
+    cv2.line(img, (width / 2, height / 2 + 10), (width / 2, height / 2 - 10), (0, 0, 255))
+
+    return img
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+
+    img = cv2.imread('template.jpg', 0)
+    img = cv2.Canny(img[0:43,0:43], 100, 200)
+    histo, _ = np.histogram(img.flatten())
+    print img.argmax()
+    cv2.imshow('test', img)
+    plt.plot(histo)
+    plt.show()
+    cv2.destroyAllWindows()
+
