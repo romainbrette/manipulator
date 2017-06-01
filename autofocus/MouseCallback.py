683183201bda0132e4ebdd87d1400bc2251465cb
achagnon <mra.chagnon@gmail.com>
2017-06-01 17:50:00 +0200

    Placing the tip now finish by x axis.

--- autofocus/MouseCallback.py
+++ autofocus/MouseCallback.py
@@ -21,9 +21,8 @@ def clic_position(event, x, y, flags, param):
             for i in range(3):
                 pos[i, 0] = param['mic'].position(i) - param['init_mic'][i]
 
-            temp = matrix([[(param['x_init'] - x + param['loc'][0]) * param['um_px']],
-                           [(param['y_init'] - y + param['loc'][1]) * param['um_px']]])
-            temp = inv(param['alpha'])*temp
+            temp = inv(param['alpha']) * matrix([[(param['x_init'] - x + param['loc'][0]) * param['um_px']],
+                                                [(param['y_init'] - y + param['loc'][1]) * param['um_px']]])
             pos[0, 0] += temp[0, 0]
             pos[1, 0] += temp[1, 0]
 
