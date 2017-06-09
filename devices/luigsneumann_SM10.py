45c313123443e9cd3c70c89e02689a5b6248b778
romainbrette <romain.brette@inserm.fr>
2017-06-09 15:22:17 +0200

    Add single step command (but not clear whether it is actually useful...)

--- devices/luigsneumann_SM10.py
+++ devices/luigsneumann_SM10.py
@@ -173,6 +173,24 @@ class LuigsNeumann_SM10(SerialDevice):
         # Send move command
         self.send_command(ID, [0xA0] + axes4 + pos, -1)
 
+    def single_step(self, axis, steps):
+        if not -127 < steps < 127:
+            raise ValueError('Steps argument has to be between '
+                             '-127 and 127 (was {})'.format(steps))
+        if steps < 0:
+            steps += 256 # two complement
+        ID = '0147'
+        data = (axis, steps)
+        self.send_command(ID, data, 0)
+
+    def set_single_step_resolution(self, axis, micro_steps):
+        if not 0 < micro_steps < 255:
+            raise ValueError('Micro steps argument has to be between '
+                             '0 and 255 (was {})'.format(micro_steps))
+        ID = '0146'
+        data = (axis, micro_steps)
+        self.send_command(ID, data, 0)
+
     def stop(self, axis):
         """
         Stop current movements on one axis.
@@ -222,4 +240,25 @@ if __name__ == '__main__':
     print(''.join(['%x' % a for a in group_address([3, 6, 9, 12, 15, 18])]))
     print(''.join(['%x' % a for a in group_address([4, 5, 6, 7, 8, 9, 10, 11, 12])]))
     sm10 = LuigsNeumann_SM10('COM3')
-    print sm10.position([1, 2, 3])
+    sm10.set_single_step_resolution(1, 10)
+
+    # sm10.absolute_move(1000, 1)
+    # sm10.wait_motor_stop([1])
+    print sm10.position_group([1, 2, 3])
+
+    sm10.single_step(1, 1)
+    sm10.wait_motor_stop([1])
+    print sm10.position_group([1, 2, 3])
+
+    sm10.single_step(1, -1)
+    sm10.wait_motor_stop([1])
+    print sm10.position_group([1, 2, 3])
+
+    sm10.single_step(1, 1)
+    sm10.single_step(1, 1)
+    sm10.wait_motor_stop([1])
+    print sm10.position_group([1, 2, 3])
+
+    sm10.single_step(1, -2)
+    sm10.wait_motor_stop([1])
+    print sm10.position_group([1, 2, 3])
