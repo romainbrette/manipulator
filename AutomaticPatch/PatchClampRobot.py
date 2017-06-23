0a299b47c1e591060a6421ac86b00392721cdd05
achagnon <mra.chagnon@gmail.com>
2017-06-23 13:45:41 +0200

    Now use step command for movement.

--- AutomaticPatch/PatchClampRobot.py
+++ AutomaticPatch/PatchClampRobot.py
@@ -338,11 +338,13 @@ class PatchClampRobot(object):
             self.step *= 2.
 
         # Move the arm
-        self.arm.relative_move(self.step, axis)
+        # self.arm.relative_move(self.step, axis)
+        self.arm.step_move(axis, self.step)
 
         # Move the platform to center the tip
         for i in range(3):
-            self.microscope.relative_move(self.mat[i, axis] * self.step, i)
+            # self.microscope.relative_move(self.mat[i, axis] * self.step, i)
+            self.microscope.step_move(i, self.mat[i, axis] * self.step)
 
         # Waiting for motors to stop
         self.arm.wait_motor_stop(axis)
@@ -360,7 +362,8 @@ class PatchClampRobot(object):
         delta = matrix('{a}; {b}'.format(a=(self.x_init - loc[0]) * self.um_px, b=(self.y_init - loc[1]) * self.um_px))
         move = self.rot_inv * delta
         for i in range(2):
-            self.microscope.relative_move(move[i, 0], i)
+            # self.microscope.relative_move(move[i, 0], i)
+            self.microscope.step_move(i, move[i, 0])
 
         self.microscope.wait_motor_stop([0, 1])
 
