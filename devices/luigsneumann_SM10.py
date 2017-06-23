b3c67a1b56c3c9abd1d076ca52b3c8fd8fdbca3c
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-06-23 13:01:57 +0200

    Re-add old single step commands for SM10

--- devices/luigsneumann_SM10.py
+++ devices/luigsneumann_SM10.py
@@ -198,7 +198,7 @@ class LuigsNeumann_SM10(SerialDevice):
         # Send move command
         self.send_command(ID, [0xA0] + axes4 + pos, -1)
 
-    def single_step(self, axis, steps):
+    def single_step_trackball(self, axis, steps):
         '''
         '''
         ID = '01E8'
@@ -206,11 +206,39 @@ class LuigsNeumann_SM10(SerialDevice):
             steps += 256
         self.send_command(ID, [axis, steps], 0)
 
-    def set_single_step_factor(self, axis, factor):
+    def set_single_step_factor_trackball(self, axis, factor):
         ID = '019F'
         if factor < 0:
             factor += 256
         data = (axis, factor)
+        self.send_command(ID, data, 0)
+
+    def single_step(self, axis, steps):
+        '''
+        Moves the given axis using the StepIncrement or StepDecrement command.
+        Using a steps argument different from 1 (or -1) simply sends multiple
+        StepIncrement/StepDecrement commands.
+        Uses distance and velocity set by `set_single_step_distance` resp.
+        `set_single_step_velocity`.
+        '''
+        if steps > 0:
+            ID = '0140'
+        else:
+            ID = '0141'
+        for _ in range(abs(steps)):
+            self.send_command(ID, [axis], 0)
+
+    def set_single_step_distance(self, axis, distance):
+        '''
+        Distance (in um) for `single_step`.
+        '''
+        ID = '044F'
+        data = [axis] + list(bytearray(struct.pack('f', distance)))
+        self.send_command(ID, data, 0)
+
+    def set_single_step_velocity(self, axis, velocity):
+        ID = '0158'
+        data = (axis, velocity)
         self.send_command(ID, data, 0)
 
     def stop(self, axis):
@@ -226,7 +254,7 @@ class LuigsNeumann_SM10(SerialDevice):
         """
         Set the current position of the axes as the zero position
         :param axes:
-        :return: 
+        :return:
         """
         # # collection command does not seem to work...
         # ID = 'A0F0'
@@ -239,7 +267,7 @@ class LuigsNeumann_SM10(SerialDevice):
     def go_to_zero(self, axis):
         """
         Make axis go to zero position
-        :return: 
+        :return:
         """
         ID = '0024'
         for axes in axis:
@@ -250,7 +278,7 @@ class LuigsNeumann_SM10(SerialDevice):
         Wait for the motor to stop
         On SM10, motors' commands seems to block
         :param axes:
-        :return: 
+        :return:
         """
 
         axes4 = [0, 0, 0, 0]
